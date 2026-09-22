2b7efa0d35ff67d84578a84ebf050253d366c0db
Taylor Jones <tejones5678@gmail.com>
2026-09-21 17:00:08 -0700

    Initial implementation of the petting zoo

---
+++ lab04/zoo.py
@@ -0,0 +1,54 @@
+def show_help():
+    print("Type 'help' to see this list again")
+    print("Type 'see' to see all the animals")
+    print("Type 'pet' followed by the animal's name to pet that animal")
+    print("Type 'bye' to leave the zoo and exit the program")
+
+def show_all_animals():
+    print("The animals in the zoo are:")
+    print("• Clover the Bunny 🐇")
+    print("• Coco the Baby Goat 🐐")
+    print("• Arno the Alligator 🐊")
+
+def pet_animal(animal):
+    if animal == "Clover":
+        print("Clover is so happy! ❤️")
+    elif animal =="Coco":
+        print("Coco the Baby Goat thanks you! 🥰")
+    elif animal == "Arno":
+        print("Actually, we cannot allow you to pet Arno. ⛔️")
+    else:
+        print("Sorry, I don't know that animal")
+
+
+print("Welcome to the Petting Zoo!")
+print("Type 'help' to get a list of all the things you can do")
+print()
+response = input("What would you like to do?")
+if response == "help":
+    show_help()
+elif response == "see":
+    show_all_animals()
+elif response.startswith("pet "):
+    animal = response[4:]
+    pet_animal(animal)
+elif response =="bye":
+    print("Goodbye!")
+else:
+    print("Sorry, I don't understand that command")
+
+keep_going = True
+while keep_going:
+    response = input("What would you like to do? ")
+    if response == "help":
+        show_help()
+    elif response == "see":
+        show_all_animals()
+    elif response.startswith("pet "):
+        animal = response[4:]
+        pet_animal(animal)
+    elif response == "bye":
+        print("Goodbye!")
+        keep_going = False
+    else:
+        print("Sorry, I don't understand that command.")
